e6ecbcb4230b2daa1bb1df85a5697fc4c95a07ac
YaseenKhan07 <114576711+YaseenKhan07@users.noreply.github.com>
2022-10-18 12:42:45 +0500

    Break and Continue Statements in PHP
    Break and Continue Statements in PHP

---
+++ break and continue statement in php.php
@@ -0,0 +1,84 @@
+<h1>FUNTER</h1>
+
+
+
+
+
+
+
+
+
+<?php 
+
+
+
+
+
+
+/*Continue Loop*/
+die();
+for($a=1;$a<11;$a++){
+    
+    if($a==5 || $a==6 || $a==7){
+    continue;}
+    echo($a);
+    echo('<br>');
+    
+}
+
+
+
+
+die();
+for($a=1;$a<11;$a++){
+    continue;
+    echo($a);
+    echo('<br>');
+    
+}
+
+
+
+
+die();
+
+
+for($a=1;$a<15;$a++){
+    echo($a);
+    echo('<br>');
+    
+    if($a==-7 || $a==15){
+        continue;
+    }
+}
+
+
+
+
+
+
+
+
+die();
+
+
+
+/*Break Loop*/
+
+
+
+for($a=1;$a<15;$a++){
+    echo($a);
+    echo('<br>');
+    
+    if($a==-7 || $a==13){
+        break;
+    }
+}
+
+
+
+
+
+
+?>
